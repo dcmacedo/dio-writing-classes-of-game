66e4999886f230f31b4276c0c14889e5f2bd48bf
Danilo Cotrim Macedo <dcotrimmacedo@gmail.com>
2023-12-09 22:15:19 -0300

    :sparkles: feat: update types of attacks

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 // Desenvolvido por Danilo C. Macedo como solução ao desafio da DIO
 
-// Aqui crio constantes que representam os ataques de cada tidpo de herói
-const MAGIA = "magia" // Mago
-const ESPADA = "espada" // Guerreiro
-const ARTESMARCIAIS = "artes marciais" // Monge
-const SHURIKEN = "shuriken" // Ninja
+// Aqui crio uma constante que representa os ataques de cada tidpo de herói
+const TipoAtaque = {
+    MAGIA: "usando magia", // Mago
+    ESPADA: "usando espada", // Guerreiro
+    ARTES_MARCIAIS: "usando artes marciais", // Monge
+    SHURIKEN: "usando shuriken" // Ninja
+} 
 
 // Aqui crio a classe que servirá de base para criar nossos heróis
-class hero{
+class Hero{
     constructor(nameHero, ageHero, typeHero="Guerreiro"){
         this.nameHero = nameHero,
         this.ageHero = ageHero,
         this.typeHero = typeHero
     }
-    atacar(){
+    Atacar(){
         if (this.typeHero == "Guerreiro") {
-            console.log(`O ${this.typeHero}, de nome ${this.nameHero}, atacou usando ${ESPADA}`)
+            console.log(`O ${this.typeHero}, de nome ${this.nameHero}, atacou ${TipoAtaque.ESPADA}`)
         }else if(this.typeHero == "Mago"){
-            console.log(`O ${this.typeHero}, de nome ${this.nameHero}, atacou usando ${MAGIA}`)
+            console.log(`O ${this.typeHero}, de nome ${this.nameHero}, atacou ${TipoAtaque.MAGIA}`)
         }else if(this.typeHero == "Monge"){
-            console.log(`O ${this.typeHero}, de nome ${this.nameHero}, atacou usando ${ARTESMARCIAIS}`)
+            console.log(`O ${this.typeHero}, de nome ${this.nameHero}, atacou ${TipoAtaque.ARTES_MARCIAIS}`)
         }else{
-            console.log(`O ${this.typeHero}, de nome ${this.nameHero}, atacou usando ${SHURIKEN}`)
+            console.log(`O ${this.typeHero}, de nome ${this.nameHero}, atacou ${TipoAtaque.SHURIKEN}`)
         }
     }
 }
 
 // Aqui criamos nossos heróis
-let scarletWitch = new hero("Wanda Maximoff", 33, "Mago")
-let tordo = new hero("Katniss Everdeen", 16)
-let mordo = new hero("Mordo", 40, "Monge")
-let bruce = new hero("Bruce Lee", 45, "Ninja")
+let scarletWitch = new Hero("Wanda Maximoff", 33, "Mago")
+let tordo = new Hero("Katniss Everdeen", 16)
+let mordo = new Hero("Barão Mordo", 40, "Monge")
+let bruce = new Hero("Bruce Lee", 45, "Ninja")
 
 // Aqui chamamos o método atacar 
-scarletWitch.atacar()
-tordo.atacar()
-mordo.atacar()
-bruce.atacar()
+scarletWitch.Atacar()
+tordo.Atacar()
+mordo.Atacar()
+bruce.Atacar()
